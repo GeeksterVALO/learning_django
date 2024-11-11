0342a7772f0c48739be6a8474f3d934e33fe1b41
kirillkurtov <kirillkurtov@gmail.com>
2024-11-11 17:14:10 +0300

    fix deploy

---
+++ hexlet-flask-example/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Flask!'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users():
+    return 'Users', 302
+
+
+@app.route('/courses/<id>')
+def courses(id):
+    return f'Course id: {id}'
+
+
+@app.route('/users/<id>')
+def show_user(id):
+    user = {
+        "id": id,
+        "name": f"user-{id}"
+    }
+    return render_template(
+        'users/show.html',
+        user=user,
+    )
