d352f7efe1099cf44a6b3c2c5e472bebd59bb5ba
kirillkurtov <kirillkurtov@gmail.com>
2024-11-14 17:01:56 +0300

    fix dotenv

--- game_web_service/settings.py
+++ game_web_service/settings.py
@@ -61,7 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'game_web_service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -71,7 +70,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -91,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -102,7 +99,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
