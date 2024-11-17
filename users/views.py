79f982136ae6fae91daab258f7da146b4a23c5de
kirillkurtov <kirillkurtov@gmail.com>
2024-11-17 23:37:44 +0300

    add edit profile

--- users/views.py
+++ users/views.py
@@ -22,6 +22,18 @@ def profile(request):
     return render(request, 'users/profile.html')
 
 @login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserEditForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('profile')
+    else:
+        form = UserEditForm(instance=request.user)
+    return render(request, 'users/edit_profile.html', {'form': form})
+
+@login_required
 def add_to_favorites(request):
     if request.method == 'POST':
         form = FavoriteGameForm(request.POST)
